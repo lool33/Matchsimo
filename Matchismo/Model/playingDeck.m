af81ea011693c856ace9362f2095657e8e4b1b2c
lool <laurentgaidon@hotmail.com>
2013-01-28 16:00:45 +0100

    implementation of class playing Deck finished

--- Matchismo/Model/playingDeck.m
+++ Matchismo/Model/playingDeck.m
@@ -7,7 +7,29 @@
 //
 
 #import "playingDeck.h"
+#import "playingCard.h"
 
 @implementation playingDeck
+
+
+-(id)init
+{
+    self = [super init];
+    
+    if(self){
+    
+        for (NSString *suit in [playingCard validSuits]) {
+            for (NSUInteger rank = 1; rank <= [playingCard maxRank]; rank++) {
+                playingCard *card = [[playingCard alloc]init];
+                card.suit = suit;
+                card.rank = rank;
+                [self addCard:card onTop:YES];
+            }
+        }
+        
+    }
+    
+    return self;
+}
 
 @end
