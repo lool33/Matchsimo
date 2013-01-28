e70fbb9b432104db6b775522b3f99f213a782ffe
lool <laurentgaidon@hotmail.com>
2013-01-28 15:53:28 +0100

    adding playingDeck class

---
+++ Matchismo/Model/playingDeck.h
@@ -0,0 +1,13 @@
+//
+//  playingDeck.h
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 28/01/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import "deck.h"
+
+@interface playingDeck : deck
+
+@end
---
+++ Matchismo/Model/playingDeck.m
@@ -0,0 +1,13 @@
+//
+//  playingDeck.m
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 28/01/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import "playingDeck.h"
+
+@implementation playingDeck
+
+@end
