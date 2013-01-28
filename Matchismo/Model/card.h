6ca3110c0b1dce6e88f46aa25c6aecabb7b856b9
lool <laurentgaidon@hotmail.com>
2013-01-28 02:01:42 +0100

    Starting create the model with the class card

---
+++ Matchismo/Model/card.h
@@ -0,0 +1,19 @@
+//
+//  card.h
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 28/01/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface card : NSObject
+
+@property(nonatomic,strong)NSString *contents;
+@property(nonatomic,readonly)NSString *imageName;
+
+
+
+
+@end
---
+++ Matchismo/Model/card.m
@@ -0,0 +1,13 @@
+//
+//  card.m
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 28/01/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import "card.h"
+
+@implementation card
+
+@end
