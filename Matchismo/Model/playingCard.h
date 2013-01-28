f5db22b71148c79de456f14aa505b709a0267d1e
lool <laurentgaidon@hotmail.com>
2013-01-28 15:13:59 +0100

    adding playingCard class and define two properties the suits and the rank of the played card

---
+++ Matchismo/Model/playingCard.h
@@ -0,0 +1,19 @@
+//
+//  playingCard.h
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 28/01/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import "card.h"
+
+@interface playingCard : card
+
+@property(nonatomic,strong) NSString *suits;
+@property(nonatomic) NSUInteger rank;
+
+
+
+
+@end
---
+++ Matchismo/Model/playingCard.m
@@ -0,0 +1,13 @@
+//
+//  playingCard.m
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 28/01/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import "playingCard.h"
+
+@implementation playingCard
+
+@end
