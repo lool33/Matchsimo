593c354c9d43faf0016951eb2529f283297ccdfa
lool <laurentgaidon@hotmail.com>
2013-01-31 02:08:03 +0100

    adding the cardMatchingGame class to the model

---
+++ Matchismo/Model/cardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  cardMatchingGame.h
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 31/01/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cardMatchingGame : NSObject
+
+@end
---
+++ Matchismo/Model/cardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  cardMatchingGame.m
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 31/01/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import "cardMatchingGame.h"
+
+@implementation cardMatchingGame
+
+@end
