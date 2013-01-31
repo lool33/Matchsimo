4d84b029e805ebbfb17e38fd6d26e7dede2f01d3
lool <laurentgaidon@hotmail.com>
2013-01-31 14:09:34 +0100

    Class card renamed to Card and ruibuid (pfffouuu)

---
+++ Matchismo/Model/Card.h
@@ -0,0 +1,23 @@
+//
+//  card.h
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 28/01/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//  Class representing a card with a content (suits and rank) and
+//  an imageName used for the display of myself
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@property(nonatomic,strong)NSString *contents;
+@property(nonatomic,readonly)NSString *imageName;
+
+@property(nonatomic,getter=isFaceUp) BOOL faceUp;
+@property(nonatomic,getter=isUnPlayable) BOOL unPlayable;
+
+-(int)match:(NSArray *)otherCards;
+
+
+@end
---
+++ Matchismo/Model/Card.m
@@ -0,0 +1,35 @@
+//
+//  card.m
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 28/01/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import "Card.h"
+
+
+@implementation Card
+
+
+//method to check if I'm (self, the instanciated card) the same
+//than some cards passed as argument here (otherCards)
+-(int)match:(NSArray *)otherCards
+{
+    
+    int score = 0;
+    
+    for (Card *card in otherCards) {
+        
+        if([card.contents isEqualToString:self.contents])
+        {
+            score = 1;
+        }
+        
+    }
+    
+    
+    return score;
+}
+
+@end
