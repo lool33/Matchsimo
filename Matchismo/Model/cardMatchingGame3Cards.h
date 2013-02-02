f5c75de68abbdfc020caff8e64d69de418f5e2ee
lool <laurentgaidon@hotmail.com>
2013-02-02 11:35:23 +0100

    adding the cardMatchingGame3Cards class

---
+++ Matchismo/Model/cardMatchingGame3Cards.h
@@ -0,0 +1,13 @@
+//
+//  cardMatchingGame3Cards.h
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 02/02/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import "cardMatchingGame.h"
+
+@interface cardMatchingGame3Cards : cardMatchingGame
+
+@end
---
+++ Matchismo/Model/cardMatchingGame3Cards.m
@@ -0,0 +1,13 @@
+//
+//  cardMatchingGame3Cards.m
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 02/02/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import "cardMatchingGame3Cards.h"
+
+@implementation cardMatchingGame3Cards
+
+@end
