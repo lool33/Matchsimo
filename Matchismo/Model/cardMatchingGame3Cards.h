3367c6003d71e1a620fc7906eced019cd73860eb
lool <laurentgaidon@hotmail.com>
2013-02-02 11:54:20 +0100

    starting the implementation of the cardMatchingGame3 class

--- Matchismo/Model/cardMatchingGame3Cards.h
+++ Matchismo/Model/cardMatchingGame3Cards.h
@@ -10,4 +10,6 @@
 
 @interface cardMatchingGame3Cards : cardMatchingGame
 
+
+
 @end
--- Matchismo/Model/cardMatchingGame3Cards.m
+++ Matchismo/Model/cardMatchingGame3Cards.m
@@ -7,7 +7,49 @@
 //
 
 #import "cardMatchingGame3Cards.h"
+#import "playingCard.h"
 
 @implementation cardMatchingGame3Cards
+
+
+-(void)flipCardAtIndex:(NSUInteger)index
+{
+    
+    //1-retrieve the card passed from the index
+    Card *card = [self cardAtIndex:index];
+    
+    //2-check if it's playable
+    if(!card.isUnPlayable){
+        
+        
+        //3-check if it's not already facedUp
+        if(!card.isFaceUp){
+          
+            //We create a string to store the result of the flip and then save it into the flipHistory
+            NSString *flipResult = nil;
+            
+            
+            //4-if not we check if turning this card faced up create a match
+            //So we check if there is some other car already faced up
+           
+            
+            
+            
+            
+            
+        }
+
+        
+    }
+        
+    
+    //So we check if there is other cards already returned
+    //but we have to managed the fact that it could have 2 other cards returned
+    //this is managed by the playingCard's method match:(NSArray *)otherCards
+
+
+    
+}
+
 
 @end
