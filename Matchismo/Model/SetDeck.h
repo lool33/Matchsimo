d7fec1b094714390aa2c3acc4b67a0962941130f
lool <laurentgaidon@hotmail.com>
2013-02-09 03:04:22 +0100

    SetDeck model class implemented

---
+++ Matchismo/Model/SetDeck.h
@@ -0,0 +1,13 @@
+//
+//  SetDeck.h
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 09/02/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import "deck.h"
+
+@interface SetDeck : deck
+
+@end
---
+++ Matchismo/Model/SetDeck.m
@@ -0,0 +1,42 @@
+//
+//  SetDeck.m
+//  Matchismo
+//
+//  Created by Laurent GAIDON on 09/02/13.
+//  Copyright (c) 2013 Laurent GAIDON. All rights reserved.
+//
+
+#import "SetDeck.h"
+#import "SetCard.h"
+
+@implementation SetDeck
+
+
+-(id)init
+{
+    self = [super init];
+    
+    if(self){
+       //here we make the deck by iterating each card property:
+        
+        for (NSString *color in [SetCard validColors]) {
+            for (NSString *shape in [SetCard validShadings]) {
+                for(NSString *symbol in [SetCard validSymbols]){
+                    for (int number = 1; number <= [SetCard maxNumber]; number ++) {
+                        SetCard *setCard = [[SetCard alloc]init];
+                        setCard.color = color;
+                        setCard.shading = shape;
+                        setCard.symbol = symbol;
+                        setCard.number = number;
+                        
+                    }
+                }
+            }
+        }
+        
+    }
+
+    return self;
+}
+
+@end
