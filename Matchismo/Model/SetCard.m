0dad27443d4c3b1dafe874fafb38c067b4f7d254
lool <laurentgaidon@hotmail.com>
2013-02-09 02:02:33 +0100

    setter and getter implemented

--- Matchismo/Model/SetCard.m
+++ Matchismo/Model/SetCard.m
@@ -10,7 +10,7 @@
 
 @implementation SetCard
 
-
+//public API implementation
 +(NSArray *)validColors
 {
     return @[@"green",@"red",@"blue"];
@@ -34,6 +34,54 @@
     
 }
 
+//setter and getter implementation
+
+@synthesize color = _color;
+@synthesize symbol = _symbol;
+
+-(void)setColor:(NSString *)color
+{
+    if(_color !=color){
+        
+        if([[SetCard validColors] containsObject:color]){
+            _color = color;
+        }
+    }
+}
+
+-(NSString *)color
+{
+    return _color ? _color : @"?";
+    
+}
+
+-(void)setSymbol:(NSString *)symbol
+{
+    if(_symbol !=symbol){
+        if([[SetCard validSymbols] containsObject:symbol]){
+            _symbol = symbol;
+        }
+    }
+}
+
+-(NSString *)symbol
+{
+    return _symbol ? _symbol : @"?";
+}
+
+-(void)setShading:(float)shading
+{
+    if(shading != _shading){
+        if(shading <= 1.0 && shading >= 0) _shading = shading;
+        if(shading < 0) _shading = 0;
+        if(shading > 1.0) _shading = 1;
+    }
+}
+
+-(void)setNumber:(int)number
+{
+    if(number <= [SetCard maxNumber]) _number = number;
+}
 
 
 @end
